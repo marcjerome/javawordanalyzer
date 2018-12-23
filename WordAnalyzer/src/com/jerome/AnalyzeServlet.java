cd9d4530050f2426061551e77119fc36905fbad7
Jerome <Jerome@DESKTOP-PUQBAPE>
2018-12-23 23:04:05 +0800

    First Commit

---
+++ WordAnalyzer/src/com/jerome/AnalyzeServlet.java
@@ -0,0 +1,28 @@
+package com.jerome;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class AnalyzeServlet extends HttpServlet{
+	
+	String word = "";
+	int option;
+	
+	
+	public void service(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		 word = request.getParameter("word");
+		 option = Integer.parseInt(request.getParameter("options"));
+		analyzeWord analyzeWord;
+		analyzeWord = new analyzeWord(word,option);
+		
+		PrintWriter out = response.getWriter();
+		
+		out.println("word: " + word);
+		out.println(analyzeWord);
+	}
+	
+	
+}
